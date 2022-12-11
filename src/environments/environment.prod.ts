977939f0058d405f71c553d17adefe979af07eb9
Sigma- <tom_wautelet@hotmail.com>
2022-12-11 21:23:19 +0100

    new features

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://infinite-harbor-32161.herokuapp.com',
-  socketUrl: 'https://infinite-harbor-32161.herokuapp.com'
+  api: 'https://infinite-harbor-32161.herokuapp.com/',
+  socketUrl: 'https://infinite-harbor-32161.herokuapp.com/'
 };
